1698186ebf619f38282bf7c6319ef4adbbde31f7
oravla5 <alvaro@auterion.com>
2024-05-07 13:13:46 +0200

    Modified print statements

--- services/serial_port/serial_port.py
+++ services/serial_port/serial_port.py
@@ -9,19 +9,20 @@ from pysbus.serial_parser import SerialParser
 
 if __name__ == "__main__":
     receiver = SBUS(SerialParser('/dev/ttyTHS0', 100000))
+    print("Opened serial port")
     receiver.begin()
 
     channels = [0] * SBUSConsts.NUM_CHANNELS
 
     try:
         while True:
+            print("Waiting for incoming communication.")
             payload_ready, failsafe, lost_frame = receiver.read(channels)
-            print("Helloooo from actual app")
             time.sleep(1)
     except:
         raise()
     finally:
-        print("byeee")
+        print("Closing serial port")
         receiver.close()
 
     receiver.close()
